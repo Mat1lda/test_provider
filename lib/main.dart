dea0bd8827cf4732e26f60051ae9ffad37b02344
Mat1lda <over2505@gmail.com>
2024-09-06 15:36:35 +0700

    change  a lil

--- lib/main.dart
+++ lib/main.dart
@@ -8,67 +8,96 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyModel>(
-      create: (context) => MyModel(),
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('My App of Provider')),
-          backgroundColor: Colors.grey,
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(20),
-                color: Colors.green[200],
-                child: Consumer<MyModel>(
-                  builder:
-                      (BuildContext context, MyModel mymodel, Widget? child) {
-                    return ElevatedButton(
-                      child: Text('Do something'),
-                      onPressed: () {
-                        mymodel.doSomething();
-                      },
-                    );
-                  },
-                ),
-                // child: Consumer<MyModel>(
-                //   builder: (context, mymodel, child) {
-                //     return ElevatedButton(
-                //       child: Text('Do something'),
-                //       onPressed: () {
-                //         mymodel.doSomething();
-                //       },
-                //     );
-                //   },
-                // ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(35),
-                color: Colors.blue[200],
-                child: Consumer<MyModel>(
-                  builder: (context, mymodel, child) {
-                    return Text(mymodel.text);
-                  },
-                ),
-              ),
-            ],
-          ),
+    var myData = MyModel();
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<MyModel>(
+          create: (BuildContext context) {
+            return MyModel();
+          },
         ),
+
+        Provider<String>(
+          create: (ctx) {
+            return "hihi";
+          },
+        ),
+
+      ],
+      child: MaterialApp(
+        home: _buildHomeScreen(),
       ),
     );
+
+    // return Provider<String>(
+    //   create: (ctx) {
+    //     return "CodeFresher - lesson 9";
+    //   },
+    //   child: ChangeNotifierProvider<MyModel>(
+    //     create: (BuildContext context) {
+    //       return MyModel();
+    //     },
+    //     child: MaterialApp(
+    //       home: _buildHomeScreen(),
+    //     ),
+    //   ),
+    // );
   }
 }
 
-class MyModel with ChangeNotifier{
+_buildHomeScreen() {
+  return Scaffold(
+    appBar: AppBar(title: Text('My App')),
+    backgroundColor: Colors.grey,
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(20),
+          color: Colors.green[200],
+          child: Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () {
+                  //myData.doSomething();
+
+                  MyModel data = Provider.of<MyModel>(context, listen: false);
+                  data.doSomething();
+
+                  String sData = Provider.of<String>(context, listen: false);
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(sData)));
+                },
+                child: Text('Click me!'),
+              );
+            },
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(35),
+          color: Colors.blue[200],
+          child: Consumer<MyModel>(
+            builder: (context, mymodel, child) {
+              return Text(mymodel.text);
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+class MyModel extends ChangeNotifier {
   String text = "Hello";
 
   void doSomething() {
-    if(text == 'World')
-      text = "Hello";
-    else{
+    if (text == "Hello")
       text = "World";
-    }
+    else
+      text = "Hello";
+
     notifyListeners();
   }
 }
